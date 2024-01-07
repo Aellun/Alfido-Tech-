d13196a6f2e8e655ff8bde6dd6127570d320115b
Aellun <okeelo.k.otieno@gmail.com>
2024-01-07 16:48:07 +0300

    portfolio

---
+++ myPortfolio/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Your additional dynamic functionality or interactions can go here
+
+    // For example, let's log a message to the console
+    console.log("Scripts loaded successfully!");
+
+    // You may add more JavaScript functionality as needed
+});
